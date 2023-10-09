ed3939ece0256092174eaf7f6b3dcb5a5e2462aa
DDongYul <comeme0101@naver.com>
2023-10-09 18:26:47 +0900

    Feat: 요구사항 4 - 로그인 및 세션에 따른 버튼 변경

---
+++ src/main/java/jwp/controller/LoginUserController.java
@@ -0,0 +1,41 @@
+package jwp.controller;
+
+import core.db.MemoryUserRepository;
+import jwp.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginUserController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("login.jsp");
+        rd.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        User loginUser = MemoryUserRepository.getInstance().findUserById(userId);
+        if (loginUser != null && loginUser.getPassword().equals(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", loginUser);
+            RequestDispatcher rd = req.getRequestDispatcher("../home.jsp");
+            rd.forward(req,resp);
+        }
+        else{
+            RequestDispatcher rd = req.getRequestDispatcher("login_failed.jsp");
+            rd.forward(req,resp);
+        }
+    }
+
+}
