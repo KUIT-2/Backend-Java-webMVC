b83b48037ff0df4144a7604a6ed7473a7df644dd
최화진 <hvvxzin99@gmail.com>
2023-10-10 13:30:55 +0900

    요구사항 2 : head, nav 중복코드 -> jspf로 include하기

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/user/list")
+@WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
