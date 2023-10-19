58b8d0b3daace55e7e28c33af8b56de832eefaec
jaeuk520 <jaeuk520@naver.com>
2023-10-20 02:59:44 +0900

    feat: login/logout

---
+++ src/main/java/jwp/controller/LoginController.java
@@ -0,0 +1,28 @@
+package jwp.controller;
+
+import jwp.model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User(req.getParameter("userId"),
+                req.getParameter("password"),
+                req.getParameter("name"),
+                req.getParameter("email"));
+        // 세션 정보 저장
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+
+        resp.sendRedirect("/");
+    }
+}
