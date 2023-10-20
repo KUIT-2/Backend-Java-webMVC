cf21b6e57cb56e916390353816254cd13d80322d
parkjiwon <ji11won@naver.com>
2023-10-20 17:34:09 +0900

    Feat : DispatcherServlet 생성

---
+++ src/main/java/jwp/DispatcherServlet.java
@@ -0,0 +1,19 @@
+package jwp;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+    }
+}
