81d9a4affd4e0ee3392771ae9ad076bf597612fb
encoreJeong <zzaibean@gmail.com>
2023-10-26 21:40:57 +0900

    Add :View.java, JspView.java 추가

---
+++ src/main/java/core/mvc/view/JspView.java
@@ -0,0 +1,11 @@
+package core.mvc.view;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JspView implements View {
+    @Override
+    public void render(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+    }
+}
